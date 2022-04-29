e1fe134e7c4a4b27074e72ed9f72a04ee2f228e6
JaniAye <janiduayeshan16@gmail.com>
2022-04-29 09:41:56 +0530

    Signup ui done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./components/home-page/home-page.component";
+import {LoginFormComponent} from "./components/login-form/login-form.component";
+import {SignUpPageComponent} from "./components/sign-up-page/sign-up-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginFormComponent},
+  {path:'signup', component:SignUpPageComponent},
+  {path:'home', component:HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[LoginFormComponent,SignUpPageComponent,HomePageComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+  routingComponents
   ],
   imports: [
     BrowserModule,
